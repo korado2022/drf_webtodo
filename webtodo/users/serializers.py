cbf8993249b401e92de8d25274e2eb24434b1344
korado2022 <99610288+korado2022@users.noreply.github.com>
2023-02-08 10:52:04 +0300

    lesson_1 My homework is ready (#1)

---
+++ webtodo/users/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework.serializers import ModelSerializer
+
+from .models import User
+
+class UserModelSerializer(ModelSerializer):
+
+
+    class Meta:
+        model = User
+        # fields = '__all__'
+        fields = ('username', 'first_name', 'last_name', 'email')
