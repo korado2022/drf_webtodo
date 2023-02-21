e7980c65dfe4cb1481d15edc592e300f9afe2e83
korado <korado_29@mail.ru>
2023-02-21 17:46:28 +0300

    My homework for lesson 6 is ready

--- webtodo/todo/views.py
+++ webtodo/todo/views.py
@@ -3,18 +3,25 @@ from rest_framework.renderers import JSONRenderer, BrowsableAPIRenderer
 
 # Create your views here.
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import DjangoModelPermissionsOrAnonReadOnly, IsAuthenticated, BasePermission
 
 from .models import Project, Todo
 from .serializers import ProjectModelSerializer, TodoModelSerializer
 
 
+# class StaffOnly(BasePermission):
+#     def has_permission(self, request, view):
+#         return request.user.is_staff
+
 class ProjectModelViewSet(ModelViewSet):
     # renderer_classes = [JSONRenderer, BrowsableAPIRenderer]
+    # permission_classes = [StaffOnly]
     queryset = Project.objects.all()
     serializer_class = ProjectModelSerializer
 
 
 class TodoModelViewSet(ModelViewSet):
     # renderer_classes = [JSONRenderer, BrowsableAPIRenderer]
+    # permission_classes = [StaffOnly]
     queryset = Todo.objects.all()
     serializer_class = TodoModelSerializer
